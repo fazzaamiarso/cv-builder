2dd9dc8fdf7965734ffc9fc1f03c30f4c866e110
fazzaamiarso <fazzaamiarso@gmail.com>
2022-01-26 03:15:20 +0700

    Install and configure react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import EditorContainer from './components/EditorContainer';
 
+import { Routes, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <EditorContainer />;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Link to="/editor" className="text-blue-500">
+            Go to Editor
+          </Link>
+        }
+      />
+      <Route path="/editor" element={<EditorContainer />} />
+    </Routes>
+  );
 }
 export default App;
