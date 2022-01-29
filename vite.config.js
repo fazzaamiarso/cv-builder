bdd00f21189f2dc2744ab5e7add7215b6719f43e
fazzaamiarso <fazzaamiarso@gmail.com>
2022-01-30 02:14:53 +0700

    Fix vite.config.js to define global

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import shimReactPdf from 'vite-plugin-shim-react-pdf';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: {},
+    'process.env': process.env,
+    Buffer: {},
+  },
   plugins: [react(), shimReactPdf()],
 });
