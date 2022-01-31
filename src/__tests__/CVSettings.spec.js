378fcce5f413b5c1f2af272181e63449cd62c441
fazzaamiarso <fazzaamiarso@gmail.com>
2022-01-31 23:56:02 +0700

    Try to fix act error and add delete section test

--- src/__tests__/CVSettings.spec.js
+++ src/__tests__/CVSettings.spec.js
@@ -1,5 +1,4 @@
 import { render, screen, within } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import CVSettings from '../components/CVSettings/CVSettings';
 import fakeData from '../testUtils/fakeData';
